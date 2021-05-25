03c5003ed58bec809b3ce1516aeff2aa17a498d6
dixondeuxyeux <dixondeuxyeux@gmail.com>
2021-05-25 19:01:35 +0000

    Prepare

--- server/server.js
+++ server/server.js
@@ -13,9 +13,9 @@ const app = express();
 // app.use(compression());
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 // db
 mongoose
@@ -35,6 +35,7 @@ app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/client/build')));
+  
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
